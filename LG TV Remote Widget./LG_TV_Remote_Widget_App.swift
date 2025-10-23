7367383af416eea3f04ed23ecf2df8369ba34744
Bardia Barabadi <b.barabadi@gmail.com>
2025-10-23 12:40:55 -0700

    Initial Commit

---
+++ LG TV Remote Widget./LG_TV_Remote_Widget_App.swift
@@ -0,0 +1,17 @@
+//
+//  LG_TV_Remote_Widget_App.swift
+//  LG TV Remote Widget.
+//
+//  Created by Bardia Barabadi on 2025-10-23.
+//
+
+import SwiftUI
+
+@main
+struct LG_TV_Remote_Widget_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
